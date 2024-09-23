45c9e9542fe5cd16f9164be809efb5b6b15ac1c2
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2024-09-23 16:25:31 +0200

    fix: Ignore namespace in enum match

--- yaserde_derive/src/de/expand_enum.rs
+++ yaserde_derive/src/de/expand_enum.rs
@@ -10,12 +10,12 @@ pub fn parse(
   root_attributes: &YaSerdeAttribute,
   generics: &Generics,
 ) -> TokenStream {
-  let namespaces_matching = root_attributes.get_namespace_matching(
-    &None,
-    quote!(enum_namespace),
-    quote!(named_element),
-    true,
-  );
+  // let namespaces_matching = root_attributes.get_namespace_matching(
+  //   &None,
+  //   quote!(enum_namespace),
+  //   quote!(named_element),
+  //   true,
+  // );
 
   let match_to_enum: TokenStream = data_enum
     .variants
@@ -56,7 +56,7 @@ pub fn parse(
         let start_depth = reader.depth();
         ::yaserde::__derive_debug!("Enum {} @ {}: start to parse {:?}", stringify!(#name), start_depth, named_element);
 
-        #namespaces_matching
+        // #namespaces_matching
 
         #[allow(unused_assignments, unused_mut)]
         let mut enum_value = ::std::option::Option::None;
